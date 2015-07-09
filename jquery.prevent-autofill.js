28020638b76c69505df52aee0d3c4512e12aa45d
theJuddson <jsharp220@gmail.com>
2015-07-09 13:49:11 -0400

    Fix numbers...

--- jquery.prevent-autofill.js
+++ jquery.prevent-autofill.js
@@ -30,17 +30,19 @@
                 char = char.toLocaleLowerCase();
             }
         } else if(isNumeric(char)){
-            switch(char){
-                case '1': return '!';
-                case '2': return '@';
-                case '3': return '#';
-                case '4': return '$';
-                case '5': return '%';
-                case '6': return '^';
-                case '7': return '&';
-                case '8': return '*';
-                case '9': return '(';
-                case '0': return ')';
+            if(shift){
+                switch(char){
+                    case '1': return '!';
+                    case '2': return '@';
+                    case '3': return '#';
+                    case '4': return '$';
+                    case '5': return '%';
+                    case '6': return '^';
+                    case '7': return '&';
+                    case '8': return '*';
+                    case '9': return '(';
+                    case '0': return ')';
+                }
             }
         } else {
             switch(keycode){
